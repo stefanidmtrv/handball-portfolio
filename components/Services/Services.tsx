ad7343894243b14eb84f7a996e2a8584df4f9991
Yoshan Mumberson <mryosh@outlook.com>
2024-09-08 12:53:15 +0100

    Implemented Services section.

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,9 +1,42 @@
-import { Title } from "@mantine/core";
+import {
+    Title,
+    Text,
+    Card,
+    SimpleGrid,
+    Container,
+    useMantineTheme,
+  } from '@mantine/core';
+  import classes from './Services.module.css';
+  import { services } from '@/app/constants/constants';
+interface ServicesCardProps {
+    title: string;
+    text: string;
+}
+
+export function ServicesCard( {title, text}: ServicesCardProps) {
+    return (
+        <Card key={title} shadow="md" radius="md" className={classes.card} padding="xl">
+            <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+                {title}
+            </Text>
+            <Text fz="sm" c="dimmed" mt="sm">
+                {text}
+            </Text>
+        </Card>
+        );
+}
 
 export function Services() {
-return (
-    <>
+    const theme = useMantineTheme();
+  
+    return (
+      <Container size="lg" py="xl">
         <Title order={1} ta="center" mt={100}>Services</Title>
-    </>
-);
-}
+        <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+          {services.map((feature) => (
+                <ServicesCard title={feature.title} text={feature.text}/>
+            ))}
+        </SimpleGrid>
+      </Container>
+    );
+  }
